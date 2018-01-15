9442ac73c4a29201e1a1b3ac714c4bf52f2a7476
Þórey Jóna Guðjónsdóttir <thoreyg13@ru.is>
2018-01-15 14:46:31 +0000

    Part 24+25 ES6 Classes

--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -13,8 +13,40 @@ class Person {
     }
 }
 
-const me = new Person('Þórey Jóna Guðjónsdóttir', 25);
-console.log(me.getDescription());
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age); // super referres to the parent class, i.e. Person
+        this.major = major;
+    }
+    hasMajor() {
+        return !!this.major;
+    }
+    getDescription() {
+        let description = super.getDescription();
+        if (this.hasMajor()) {
+            description += ` Their major is ${this.major}.`;
+        }
+        return description;
+    }
+}
 
-const other = new Person();
-console.log(other.getDescription());
+class Traveller extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+
+    getGreeting() {
+        let greeting = super.getGreeting();
+        if (this.homeLocation) {
+            greeting += ` I'm visiting from ${this.homeLocation}.`;
+        }
+        return greeting;
+    }
+}
+
+const me = new Traveller('Þórey Jóna Guðjónsdóttir', 25, 'Reykjavík');
+console.log(me.getGreeting());
+
+const other = new Traveller();
+console.log(other.getGreeting());
