8d4e0ca033f2ce8479cd9f5183fc478371679773
Þórey Jóna Guðjónsdóttir <thoreyg13@ru.is>
2018-01-20 12:52:24 +0000

    Part 32 & 33, Set state to counter app

--- indecision-app/src/playground/counter-example.js
+++ indecision-app/src/playground/counter-example.js
@@ -1,4 +1,50 @@
-let count = 0;
+
+
+class Counter extends React.Component {
+    constructor(props) {
+        super(props); // Have to call super, otherwise we break things
+        this.handleAddOne = this.handleAddOne.bind(this);
+        this.handleMinusOne = this.handleMinusOne.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+        this.state = {
+            count: 0
+        };
+    }
+    handleAddOne() {
+        this.setState((prevState) => {
+            return {
+                count: prevState.count + 1
+            }
+        });
+    }
+    handleMinusOne() {
+        this.setState((prevState) => {
+            return {
+                count: prevState.count - 1
+            }
+        });
+    }
+    handleReset() {
+        this.setState(() => {
+            return {
+                count: 0
+            }
+        });
+    }
+    render() {
+        return (
+            <div>
+            <h1>Count: {this.state.count}</h1>
+            <button onClick={this.handleAddOne}>+1</button>
+            <button onClick={this.handleMinusOne}>-1</button>
+            <button onClick={this.handleReset}>reset</button>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Counter />, document.getElementById('app'));
+/*let count = 0;
 const addOne = () => {
     count++;
     renderCounterApp();
@@ -38,4 +84,4 @@ const renderCounterApp = () => {
     ReactDOM.render(templateTwo, appRoot);
 }
 
-renderCounterApp();
+renderCounterApp();*/
