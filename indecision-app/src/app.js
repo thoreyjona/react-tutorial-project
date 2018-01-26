fb433b6f6762fd9c9c78fafe480271a902b01ad0
thoreyjona <thorey@travelade.com>
2018-01-26 22:02:17 +0000

    Stateless functional components

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,3 +1,4 @@
+// Stateless functional component
 // With ES6 classes we can have no methods defined, with React components we have to define render 
 class IndecisionApp extends React.Component {
     constructor(props) {
@@ -55,47 +56,42 @@ class IndecisionApp extends React.Component {
     }
 }
 
-class Header extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>{this.props.title}</h1>
-                <h2>{this.props.subtitle}</h2>           
-            </div>
-        );
-    } 
+const Header = (props) => {
+  return (
+    <div>
+        <h1>{props.title}</h1>
+        <h2>{props.subtitle}</h2>           
+    </div>
+  );
 }
 
-class Action extends React.Component{
-    render() {
-        return (
-            <div>
-            <button 
-            onClick={this.props.handlePick}
-            disabled={!this.props.hasOptions}
-            >
-            What should I do?
-            </button>
-            </div>
-        );
-    }
+const Action = (props) => {
+  return (
+    <div>
+      <button 
+        onClick={props.handlePick}       
+        disabled={!props.hasOptions}
+      >
+      What should I do?
+      </button>
+    </div>
+  );
 }
+
 
 //Options is getting rerendered with a new set of props.
 //Component like options can not change its own props but new props value
 // can be passed from parents and then it is rerendering
-class Options extends React.Component {
-    render() {
-        return (
-            <div>
-            <button onClick={this.props.handleDeleteOptions}>Remove All</button>
-                {
-                    this.props.options.map((option) => <Option key={option} optionText={option}/>)
-                }                
-            </div>
-        );
 
-    }
+const Options = (props) => {
+  return (
+    <div>
+    <button onClick={props.handleDeleteOptions}>Remove All</button>
+        {
+            props.options.map((option) => <Option key={option} optionText={option}/>)
+        }                
+    </div>
+  );
 }
 
 class AddOption extends React.Component {
@@ -130,14 +126,12 @@ class AddOption extends React.Component {
     }
 }
 
-class Option extends React.Component {
-    render() {
-        return (
-            <div>
-            Option: {this.props.optionText}
-            </div>
-        );
-    }
+const Option = (props) => {
+  return (
+    <div>
+    Option: {props.optionText}
+    </div>
+  );
 }
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
