e514e35c1f8cfedc3e8198a04b94cdb6cc129600
thoreyjona <thorey@travelade.com>
2018-01-29 18:47:19 +0000

    Part 63. Refactoring stateless functional components

--- indecision-app/src/components/Option.js
+++ indecision-app/src/components/Option.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
-const Option = (props) => {
-    return (
-      <div>
-        Option: {props.optionText}
-        <button 
-        onClick={(e) => {
-          props.handleDeleteOption(props.optionText);
-        }}
-        
-        >
-        Remove</button>
-      </div>
-    );
-  };
+const Option = (props) => (
+  <div>
+    Option: {props.optionText}
+    <button 
+    onClick={(e) => {
+      props.handleDeleteOption(props.optionText);
+    }}
+    
+    >
+    Remove</button>
+  </div>
+);
 
-  export default Option;
+export default Option;
--- indecision-app/src/components/Options.js
+++ indecision-app/src/components/Options.js
@@ -4,21 +4,19 @@ import Option from './Option';
 //Options is getting rerendered with a new set of props.
 //Component like options can not change its own props but new props value
 // can be passed from parents and then it is rerendering
-const Options = (props) => {
-    return (
-      <div>
-      <button onClick={props.handleDeleteOptions}>Remove All</button>
-        {props.options.length === 0 && <p>Please add an option to get started</p>}
-        {
-              props.options.map((option) => (
-              <Option key={option} 
-              optionText={option}
-              handleDeleteOption={props.handleDeleteOption}
-              />
-            ))
-        }                
-      </div>
-    );
-  }
+const Options = (props) => (
+  <div>
+  <button onClick={props.handleDeleteOptions}>Remove All</button>
+    {props.options.length === 0 && <p>Please add an option to get started</p>}
+    {
+          props.options.map((option) => (
+          <Option key={option} 
+          optionText={option}
+          handleDeleteOption={props.handleDeleteOption}
+          />
+        ))
+    }                
+  </div>
+);
 
-  export default Options;
+export default Options;
