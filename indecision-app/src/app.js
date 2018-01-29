71edc881e63a1bcbaf77f9da4a453e79e68d0732
thoreyjona <thorey@travelade.com>
2018-01-29 19:57:50 +0000

    Part 65. Architecture and header styles

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -3,6 +3,4 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 import './styles/styles.scss';
 
-// Children Prop (built in prop)
-// We can pass JSX into a component and that component can choose to use it if it wants
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
