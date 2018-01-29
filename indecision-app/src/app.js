f1590965ac13db1612cca54982385d55e1e98187
thoreyjona <thorey@travelade.com>
2018-01-29 18:01:33 +0000

    Part 60. Passing children to component

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,33 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+const Layout = (props) => {
+    return (
+        <div>
+            <p>header</p>
+            {props.children}
+            <p>footer</p>
+        </div>
+    );
+};
 
-class OldSyntax {
-    constructor() {
-        this.name = "Mike";
-        this.getGreeting = this.getGreeting.bind(this);
-    }
-    getGreeting() {
-        return `Hi. My name is ${this.name}`;
-    }
-}
-
-// Error!
-const oldSyntax = new OldSyntax();
-const greeting = oldSyntax.getGreeting;
-console.log(greeting());
-
-// ------------
-
-class NewSyntax {
-    name = "Jen";
-    getGreeting = () => {
-        return `Hi. My name is ${this.name}`;
-    }
-}
-
-// No error!!
-const newSyntax = new NewSyntax();
-const newGetGreeting = newSyntax.getGreeting;
-console.log(newGetGreeting());
+// Children Prop (built in prop)
+// We can pass JSX into a component and that component can choose to use it if it wants
+ReactDOM.render((
+<Layout>
+<div>
+    <h1>Page Title</h1>
+    <p>This is my page</p>
+</div>
+</Layout>
+), document.getElementById('app'));
