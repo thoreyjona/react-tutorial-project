ae659bdf180f35ed49c89ba77d9473c8a065dec5
thoreyjona <thorey@travelade.com>
2018-02-04 11:51:33 +0000

    Part 89. ES6 Array Destructuring

--- expensify-app/src/playground/destructuring.js
+++ expensify-app/src/playground/destructuring.js
@@ -1,3 +1,6 @@
+//
+//  Object Destructuring
+//
 const person = {
     name: 'Þórey',
     age: 25,
@@ -38,3 +41,20 @@ const book = {
 const {name: publisherName = 'Self-Published'} = book.publisher;
 
 console.log(publisherName);
+
+
+//
+// Array destructuring
+//
+
+const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147'];
+
+const [, city2, state = 'New York'] = address;
+
+console.log(`You are in ${city2} ${state}.`);
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75'];
+
+const [coffee, , medium] = item;
+
+console.log(`A medium ${coffee} costs ${medium}`);
