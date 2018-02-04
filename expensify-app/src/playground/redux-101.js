23b47cf1e2770f78ccb53987a97a6c2247be6b7e
thoreyjona <thorey@travelade.com>
2018-02-04 11:15:23 +0000

    Part 86. Dispatching Actions

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -1,7 +1,44 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }) => {
-    return state;
+const store = createStore((state = { count: 0 }, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                count: state.count + 1
+            };
+        case 'DECREMENT':
+            return {
+                count: state.count - 1
+            };
+        case 'RESET':
+            return {
+                count: 0
+            }
+        default:
+            return state;
+    }
 });
 
+//Actions - than an object that gets sent to the store
+
+//I'd like to increment the count
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+store.dispatch({
+    type: 'RESET'
+});
+
+store.dispatch({
+    type: 'DECREMENT'
+});
+
+
+
+// I'd like to decr
 console.log(store.getState());
