8f87e415a607b11a9e14593cb60a7af83110fe78
thoreyjona <thorey@travelade.com>
2018-02-10 16:13:35 +0000

    Part 104. Dropdown for picking sortBy

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -11,8 +11,10 @@ import getVisibleExpenses from './selectors/expenses';
 
 const store = configureStore();
 
-store.dispatch(addExpense({description : 'Water bill', amount: 100, createdAt: 21000}));
-store.dispatch(addExpense({description : 'Gas bill', amount: 500, createdAt: 21000}));
+store.dispatch(addExpense({description : 'Water bill', amount: 4500}));
+store.dispatch(addExpense({description : 'Gas bill', createdAt: 1000}));
+store.dispatch(addExpense({description : 'Rent', amount: 109500,}));
+
 
 store.dispatch(setTextFilter('water'));
 
