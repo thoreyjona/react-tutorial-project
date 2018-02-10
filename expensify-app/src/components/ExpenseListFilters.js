57099c09e47437c8ce1cc809f8a918668b6eb6af
thoreyjona <thorey@travelade.com>
2018-02-10 16:39:06 +0000

    Part 105. Create Expense Add/Edit Form

--- expensify-app/src/components/ExpenseListFilters.js
+++ expensify-app/src/components/ExpenseListFilters.js
@@ -13,11 +13,11 @@ const ExpenseListFilters = (props) => (
         <select 
             value={props.filters.sortBy} 
             onChange={(e) => {
-            if(e.target.value === 'date') {
-                props.dispatch(sortByDate());
-            } else if (e.target.value === 'amount') {
-                props.dispatch(sortByAmount());
-            }
+                if(e.target.value === 'date') {
+                    props.dispatch(sortByDate());
+                } else if (e.target.value === 'amount') {
+                    props.dispatch(sortByAmount());
+                }
         }}>
             <option value="date">Date</option>
             <option value="amount">Amount</option>
