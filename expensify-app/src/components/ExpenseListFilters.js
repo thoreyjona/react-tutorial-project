5cf9549bd0e3c110c5a2084edd5730d11efe031e
thoreyjona <thorey@travelade.com>
2018-02-11 12:58:38 +0000

    Part 110. Filtering by Dates

--- expensify-app/src/components/ExpenseListFilters.js
+++ expensify-app/src/components/ExpenseListFilters.js
@@ -1,29 +1,56 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { setTextFilter, sortByAmount, sortByDate } from '../actions/filters';
+import { DateRangePicker } from 'react-dates';
+import { setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate } from '../actions/filters';
 
-const ExpenseListFilters = (props) => (
-    <div>
+// If we use stateless functional components we don't need to use this, 
+// when we changed to a class we need to add this in front of everything
+class ExpenseListFilters extends React.Component {
+    state = {
+        calendarFocused: null
+    };
+    onDatesChange = ({startDate, endDate}) => {
+        this.props.dispatch(setStartDate(startDate));
+        this.props.dispatch(setEndDate(endDate));
+    };
+    onFocusChange = (calendarFocused) => {
+        this.setState(() => ({ calendarFocused }));
+    }
+    render() {
+        return (
+        <div>
         <input 
             type="text" 
-            value={props.filters.text} 
+            value={this.props.filters.text} 
             onChange={(e) => {
-            props.dispatch(setTextFilter(e.target.value));
+            this.props.dispatch(setTextFilter(e.target.value));
         }}/>
         <select 
-            value={props.filters.sortBy} 
+            value={this.props.filters.sortBy} 
             onChange={(e) => {
                 if(e.target.value === 'date') {
-                    props.dispatch(sortByDate());
+                    this.props.dispatch(sortByDate());
                 } else if (e.target.value === 'amount') {
-                    props.dispatch(sortByAmount());
+                    this.props.dispatch(sortByAmount());
                 }
         }}>
             <option value="date">Date</option>
             <option value="amount">Amount</option>
         </select>
+        <DateRangePicker 
+            startDate={this.props.filters.startDate}
+            endDate={this.props.filters.endDate}
+            onDatesChange={this.onDatesChange}
+            focusedInput={this.state.calendarFocused}
+            onFocusChange={this.onFocusChange}
+            showClearDates={true}
+            numberOfMonths={1}
+            isOutsideRange={() => false}
+        />
     </div>
-);
+        );
+    }
+}
 
 const mapStateToProps = (state) => {
     return {
