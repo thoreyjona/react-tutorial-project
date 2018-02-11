0385e7269d35eeeeee8a1fa9a3d82cdaf5b54670
thoreyjona <thorey@travelade.com>
2018-02-11 12:18:44 +0000

    Part 109. Redux Dev Tools

--- expensify-app/src/store/configureStore.js
+++ expensify-app/src/store/configureStore.js
@@ -5,9 +5,9 @@ import filtersReducer from '../reducers/filters';
 export default () => {
     const store = createStore(combineReducers({
         expenses: expensesReducer,
-        filters: filtersReducer
-        
-    })
+        filters: filtersReducer  
+        }),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     );
     return store;
 }
