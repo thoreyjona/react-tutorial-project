3797a415827a41587677631d3a1a3c7ce71d38f0
thoreyjona <thorey@travelade.com>
2018-02-17 14:55:05 +0000

    Part 113. Testing Ecpenses Action Generators

---
+++ expensify-app/src/tests/actions/expenses.test.js
@@ -0,0 +1,50 @@
+import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+
+test('should set up remove expense action object', () => {
+    const action = removeExpense({ id : '123abc' });
+    expect(action).toEqual({
+        type: 'REMOVE_EXPENSE',
+        id: '123abc'
+    });
+});
+
+test('should set up remove expense action object', () => {
+    const action = editExpense( '123abc', { note: 'New note value' });
+    expect(action).toEqual({
+        type: 'EDIT_EXPENSE',
+        id: '123abc',
+        updates: { note: 'New note value'}
+    });
+});
+
+test('Should set up add expense action object with provided values', () => {
+    const expenseData = {
+        description: 'Rent',
+        amount: 109500,
+        createdAt: 1000,
+        note: 'This was last months rent'
+    };
+    const action = addExpense(expenseData);
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            ...expenseData,
+            id: expect.any(String)
+        }
+    })
+});
+
+test('Should set up add expense action object with default values', () => {
+    const action = addExpense();
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            description: '',
+            note: '',
+            amount: 0,
+            createdAt: 0,
+            id: expect.any(String)
+        }
+    })
+    
+});
