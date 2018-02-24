c52d813b8b93897f81dc7721b76fb4ad3ca3febf
thoreyjona <thorey@travelade.com>
2018-02-24 13:01:20 +0000

    Part 119. Enzyme

--- expensify-app/src/tests/components/Header.test.js
+++ expensify-app/src/tests/components/Header.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
 import Header from '../../components/Header';
+import { shallow } from 'enzyme';
 
 test('Should render Header correctly', () => {
-    const renderer = new ReactShallowRenderer();
-    renderer.render(<Header />);
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
+    const wrapper = shallow(<Header />);
+    expect(wrapper).toMatchSnapshot();
+
+    // expect(wrapper.find('h1').length).toBe(1);
+    // expect(wrapper.find('h1').text()).toBe('Expensify');
+    // const renderer = new ReactShallowRenderer();
+    // renderer.render(<Header />);
+    // expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
