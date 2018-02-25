c6836b4616897e78b153843293f55cafdaf15c08
thoreyjona <thorey@travelade.com>
2018-02-25 13:51:01 +0000

    Part 120. Snapshot testing with dynamic components

--- expensify-app/src/components/ExpenseListItem.js
+++ expensify-app/src/components/ExpenseListItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const ExpenseListItem = ({ dispatch, id, description, amount, createdAt}) => (
+const ExpenseListItem = ({ id, description, amount, createdAt}) => (
     <div>
         <Link to={`/edit/${id}`}>
             <h3>{description}</h3>
