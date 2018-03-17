e3691debdcd9daf4d506eb145a13ad2c1ae88862
thoreyjona <thorey@travelade.com>
2018-03-17 10:18:01 +0000

    Part 143. Writing to the database

--- expensify-app/src/firebase/firebase.js
+++ expensify-app/src/firebase/firebase.js
@@ -12,6 +12,23 @@ const config = {
 
   firebase.initializeApp(config);
 
-  firebase.database().ref().set({
-    name: 'Þórey Guðjónsdóttir'
+ const database = firebase.database();
+
+ database.ref().set({
+    name: 'Þórey Guðjónsdóttir',
+    age: 25,
+    isSingle: false,
+    location: {
+        city: 'Reykjavík',
+        country: 'Iceland'
+    }
   });
+
+  // database.ref().set('This is my data');
+
+  database.ref('age').set(26);
+  database.ref('location/city').set('Akureyri');
+  database.ref('attributes').set({
+      height: 161,
+      weight: 60
+  })
