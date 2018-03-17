b4f1734cb450769020bbd724ed388af31982c81e
thoreyjona <thorey@travelade.com>
2018-03-17 10:40:30 +0000

    Part 144. ES6 Promises

---
+++ expensify-app/src/playground/promises.js
@@ -0,0 +1,19 @@
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+         /* resolve({
+            name: 'Þórey',
+            age: 25
+        });*/ 
+        reject('Something went wrong');
+    }, 1500);
+});
+
+console.log('before');
+
+promise.then((data) => {
+    console.log(data);
+}).catch((error) => {
+    console.log('error: ', error);
+});
+
+console.log('after');
