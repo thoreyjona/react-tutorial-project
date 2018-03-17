ff6677ae7454f27c6a23ae059975d55896838272
thoreyjona <thorey@travelade.com>
2018-03-17 11:02:22 +0000

    Part 145. Promises with firebase

--- expensify-app/src/firebase/firebase.js
+++ expensify-app/src/firebase/firebase.js
@@ -22,13 +22,21 @@ const config = {
         city: 'Reykjavík',
         country: 'Iceland'
     }
+  }).then(() => {
+      console.log('Data is saved!');
+  }).catch((e) => {
+    console.log('This failed.', e);
   });
 
   // database.ref().set('This is my data');
 
-  database.ref('age').set(26);
-  database.ref('location/city').set('Akureyri');
+  // database.ref('age').set(26);
+  // database.ref('location/city').set('Akureyri');
   database.ref('attributes').set({
       height: 161,
       weight: 60
+  }).then(() => {
+      console.log('DATA IS SAVED!');
+  }).catch((e) => {
+    console.log('OOOPS something went wrong!', e);
   })
