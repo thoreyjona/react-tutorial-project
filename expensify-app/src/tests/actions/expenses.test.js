095342d9ee700d1aa7cfc318f573757c761ac60d
thoreyjona <thorey@travelade.com>
2018-03-31 15:09:00 +0000

    Part 154. Testing Async Redux Actions: Part II

--- expensify-app/src/tests/actions/expenses.test.js
+++ expensify-app/src/tests/actions/expenses.test.js
@@ -2,6 +2,7 @@ import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { startAddExpense, addExpense, editExpense, removeExpense } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
+import database from '../../firebase/firebase';
 
 const createMockStore = configureMockStore([thunk]);
 
@@ -40,15 +41,46 @@ test('should add expense to database and store', (done) => {
     };
 
     store.dispatch(startAddExpense(expenseData)).then(() => {
-        expect(1).toBe(2);
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: 'ADD_EXPENSE',
+            expense: {
+                id: expect.any(String),
+                ...expenseData
+            }
+        });
+        return database.ref(`expenses/${actions[0].expense.id}`).once('value');
+    }).then((snapshot) => {
+        expect(snapshot.val()).toEqual(expenseData);
         done();
     });
 });
 
 // done makes the test wait until everything is finished. Otherwise always passing because asynchronous
 
-test('should add expense with defaults to database and store', () => {
+test('should add expense with defaults to database and store', (done) => {
+    const store = createMockStore({});
+    const expenseDefaults = {
+        description: '', 
+        note: '', 
+        amount: 0, 
+        createdAt: 0
+    };
 
+    store.dispatch(startAddExpense({expenseData})).then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: 'ADD_EXPENSE',
+            expense: {
+                id: expect.any(String),
+                ...expenseDefaults
+            }
+        });
+        return database.ref(`expenses/${actions[0].expense.id}`).once('value');
+    }).then((snapshot) => {
+        expect(snapshot.val()).toEqual(expenseDefaults);
+        done();
+    });
 });
 
 /* test('Should set up add expense action object with default values', () => {
